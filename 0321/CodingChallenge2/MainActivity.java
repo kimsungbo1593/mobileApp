d006a59ea49792a6a70736883bc4900dc4621573
kimsungbo1593 <80305774+kimsungbo1593@users.noreply.github.com>
2025-03-21 21:24:10 +0900

    Add files via upload

---
+++ 0321/CodingChallenge2/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.codingch2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView TextView2;
+    private int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView2 = findViewById(R.id.TextView2);
+
+    }
+
+    public void plus(View view) {
+        count++;
+        TextView2.setText(String.valueOf(count));
+    }
+
+    public void minus(View view) {
+        count--;
+        TextView2.setText(String.valueOf(count));
+    }
+
+}
