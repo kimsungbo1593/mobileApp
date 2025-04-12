76f87305f511311c44cca68d6559a38d2b9a774c
kimsungbo1593 <80305774+kimsungbo1593@users.noreply.github.com>
2025-04-13 01:45:37 +0900

    Add files via upload

---
+++ 0411/Chapter6/Splash/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.splash;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.login);
+
+        //로그인 화면의 레이아웃을 정의하고 필요한 로직을 추가
+    }
+}
