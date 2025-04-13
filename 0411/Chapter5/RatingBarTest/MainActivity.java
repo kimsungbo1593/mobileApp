a7d63d0503c96909a74bcb5a1f16915466dd45f6
kimsungbo1593 <80305774+kimsungbo1593@users.noreply.github.com>
2025-04-13 16:14:07 +0900

    Add files via upload

---
+++ 0411/Chapter5/RatingBarTest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.ratingbartest;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private RatingBar ratingBar;
+    private TextView value;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        value = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float rating = ratingBar.getRating();
+                value.setText(String.valueOf("SCORE = " + rating));
+            }
+        });
+    }
+}
