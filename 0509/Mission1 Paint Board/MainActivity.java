7aae86ae10f80b447a12ad17dcb3896db91a936d
kimsungbo1593 <80305774+kimsungbo1593@users.noreply.github.com>
2025-05-09 14:53:03 +0900

    Add files via upload

--- 0509/Mission1 Paint Board/MainActivity.java
+++ 0509/Mission1 Paint Board/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.mission1paintboard;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.SeekBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,33 +17,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         drawingView = findViewById(R.id.drawing_view);
 
-        /* ---- 색상 버튼 ---- */
-        Button btnRed   = findViewById(R.id.btn_red);
-        Button btnBlue  = findViewById(R.id.btn_blue);
-        Button btnGreen = findViewById(R.id.btn_black);
+        // 색상 버튼
+        Button btnRed = findViewById(R.id.btn_red);
+        Button btnBlue = findViewById(R.id.btn_blue);
+        Button btnBlack = findViewById(R.id.btn_black);
 
         btnRed.setOnClickListener(v -> drawingView.setPaintColor(Color.RED));
         btnBlue.setOnClickListener(v -> drawingView.setPaintColor(Color.BLUE));
-        btnGreen.setOnClickListener(v -> drawingView.setPaintColor(Color.BLACK));
+        btnBlack.setOnClickListener(v -> drawingView.setPaintColor(Color.BLACK));
 
-        /* ---- 굵기 SeekBar ---- */
-        SeekBar seekStroke = findViewById(R.id.seek_stroke);
-        seekStroke.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                // progress 가 0 이면 아무것도 안 보일 수 있으니 최소값 보정
-                float width = progress < 1 ? 1 : progress;
-                drawingView.setStrokeWidth(width);
-            }
-            @Override public void onStartTrackingTouch(SeekBar seekBar) { }
-            @Override public void onStopTrackingTouch(SeekBar seekBar) { }
-        });
-
+        // 액션 버튼
         Button btnClear = findViewById(R.id.btn_clear);
         btnClear.setOnClickListener(v -> drawingView.clearCanvas());
 
         Button btnUndo = findViewById(R.id.btn_undo);
         btnUndo.setOnClickListener(v -> drawingView.undoLast());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.stroke_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.stroke_thin) {
+            drawingView.setStrokeWidth(4f);
+            return true;
+        } else if (item.getItemId() == R.id.stroke_medium) {
+            drawingView.setStrokeWidth(8f);
+            return true;
+        } else if (item.getItemId() == R.id.stroke_thick) {
+            drawingView.setStrokeWidth(16f);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
